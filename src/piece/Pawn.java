f6d49bc184112842b4ac82dc26c43a2c85b4bbe7
Preethesh16 <preetheshcarvalho57@gmail.com>
2024-11-13 02:31:53 +0530

    pieces

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -16,4 +16,25 @@ public class Pawn extends Piece {
 		}
 	}
 
+	public boolean canMove(int targetCol,int targetRow) {
+		if(isWithinBoard(targetCol,targetRow)&& isSameSquare(targetCol,targetRow)==false) {
+		//define the move value based on its color
+			int moveValue;
+			if(color==GamePanel.WHITE) {
+				moveValue=-1;
+			}
+			else {
+				moveValue=1;
+			}
+			//check hitting piece
+			hittingP = getHittingP(targetCol, targetRow);
+			//1 square for movement
+			if(targetCol==preCol &&  targetRow ==preRow + moveValue && hittingP==null)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+	
 }
